82688ff2a232025b4d2d97acdb8a9db2d6af6d2c
Fennec310 <jhl0310@hotmail.com>
2021-01-15 02:04:35 +0900

    clone version
    clone version

---
+++ app.js
@@ -0,0 +1,124 @@
+const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave");
+
+const INITIAL_COLOR = "#2c2c2c";
+const CANVAS_SIZE = 700;
+
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
+ctx.lineWidth = 2.5; //line size will be 2.5px
+//ctx.fillStyle = "red";
+//ctx.filRect
+
+let painting = false;
+let filling = false;
+
+function startPainting() {
+  painting = true;
+}
+
+function stopPainting() {
+  painting = false;
+}
+
+function onMouseMove(event) {
+  const x = event.offsetX;
+  const y = event.offsetY;
+
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
+
+  //console.log(event);
+  //console.log(x, y);
+}
+
+function handleColorClick(event) {
+  const color = event.target.style.backgroundColor;
+  ctx.strokeStyle = color;
+  //console.log(event.target.style.backgroundColor);
+  ctx.fillStyle = color;
+}
+
+function handleRangeChange(event) {
+  const size = event.target.value;
+  ctx.lineWidth = size;
+  // console.log(event.target.value);
+}
+
+function handleModeClick(event) {
+  if (filling === true) {
+    filling = false;
+    mode.innerText = "Fill";
+  } else {
+    filling = true;
+    mode.innerText = "Paint";
+  }
+}
+
+function handleCanvasClick(event) {
+  if (filling) {
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  }
+}
+
+function handleContextMenu(event) {
+  event.preventDefault();
+  //preventDefault(): 마우스 우 클릭시 나타나는 메뉴 없도록 한다.
+  //console.log(event);
+}
+
+function handleSaveClick(event) {
+  const image = canvas.toDataURL("image/jpeg");
+  //toDataURL() 빈칸으로 놔두면 default 값은 png.
+  //console.log(image);
+  const link = document.createElement("a");
+  //link that doesn't exist.
+  //download is an attribute of anchor tag <a>. you can use <a download=""> instead of <a href="">
+  // <a download=""> download attribute instruct browsers to download URL instead of navigating to it.
+  link.href = image;
+  link.download = "PaintJS[Export]";
+  //console.log(link);
+  link.click();
+}
+
+if (canvas) {
+  canvas.addEventListener("mousemove", onMouseMove);
+  canvas.addEventListener("mousedown", startPainting);
+  //mousedown = event when you click your mouse
+  canvas.addEventListener("mouseup", stopPainting);
+  canvas.addEventListener("mouseleave", stopPainting);
+  canvas.addEventListener("click", handleCanvasClick);
+  canvas.addEventListener("contextmenu", handleContextMenu);
+  //contextmenu 는 마우스 우클릭했을때 나타나는 메뉴
+}
+
+//console.log(Array.from(colors));
+Array.from(colors).forEach((color) =>
+  color.addEventListener("click", handleColorClick)
+);
+
+if (range) {
+  range.addEventListener("input", handleRangeChange);
+}
+
+if (mode) {
+  mode.addEventListener("click", handleModeClick);
+}
+
+if (saveBtn) {
+  saveBtn.addEventListener("click", handleSaveClick);
+}
